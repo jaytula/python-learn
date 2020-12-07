1b4e4c1e362c7d799c035b770aed18db078e237a
Jay T <jaytula@gmail.com>
2020-12-06 20:47:53 -0800

    Working with numbers. Up to 48:26

---
+++ working-with-numbers.py
@@ -0,0 +1,55 @@
+from math import *
+
+print(2)
+
+print(2.0987)
+
+# Negative numbers
+print(-3.5)
+
+# Adding
+print(3 + 4.5)
+
+# Multiply
+print(3 * 8)
+
+# Parens
+print(3 * (4+5))
+
+# Mod
+print(10 % 3)
+
+# Store in variables
+my_num = 5
+print(my_num)
+
+# Convert to string
+print(str(my_num))
+
+# Helpful with concatenating
+print(str(my_num) + " is a number")
+
+# Absolute value
+print(abs(-5))
+
+# Power
+print(pow(3, 2))
+
+# Max
+print(max(4, 6))
+
+# Min
+print(min(4, 6))
+
+# Round
+print(round(3.2))
+print(round(3.7))
+
+# floor (from math)
+print(floor(3.9))
+
+# ceil (from math)
+print(ceil(3.1))
+
+# sqrt (from math)
+print(sqrt(36))
