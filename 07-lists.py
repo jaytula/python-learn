63e2db556a8be9b957babb80217c9486ae8760fe
Jay T <jaytula@gmail.com>
2020-12-10 01:33:31 -0800

    Add 07-list.py. Up to 1:10:44

---
+++ 07-lists.py
@@ -0,0 +1,19 @@
+# A list
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+
+# Print list
+print(friends)
+
+# Print out an element
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+
+# Slicing a list
+print(friends[1:])
+print(friends[1:3])
+
+# Modification
+friends[1] = "Mike"
+print(friends)
