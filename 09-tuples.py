b5b644ac16e44f46bd8642effa01d772fe6eec64
Jay T <jaytula@gmail.com>
2020-12-12 01:27:13 -0800

    Add 09-tuples.py.  Up to 1:24:15

---
+++ 09-tuples.py
@@ -0,0 +1,10 @@
+# tuple is similar to a list
+coordinates = (4, 5)
+print(coordinates[0])
+print(coordinates[1])
+
+# tuples are immutable
+# This would error
+#coordinates[0] = 1
+
+# Use when you want data that can't be changed.
