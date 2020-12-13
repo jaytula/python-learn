bb8082bcf930f81c4713911bbddf723ea4b95468
Jay T <jaytula@gmail.com>
2020-12-13 12:46:32 -0800

    Add file 10-functions.py. 1:34:11 in video

---
+++ 10-functions.py
@@ -0,0 +1,12 @@
+# 10. Functions
+
+# Note function body is indented
+# Has one parameter `name`
+def say_hi(name, age):
+    print("Hello " + name + ", you are " + str(age))
+
+
+print("Top")
+say_hi("Mike", 35)
+say_hi("Steve", 42)
+print("Bottom")
