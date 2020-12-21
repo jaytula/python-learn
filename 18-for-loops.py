6fa8721f85b59918909e6eebef0085cc1472dcb6
Jay T <jaytula@gmail.com>
2020-12-21 01:18:30 -0800

    Add 18-for-loops.py. In 2:41:20.

---
+++ 18-for-loops.py
@@ -0,0 +1,18 @@
+# for loops
+
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["Jim", "Karen", "Kevin"]
+for friend in friends:
+    print(friend)
+
+for index in range(10):
+    print(index)
+
+for index in range(3, 9):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
