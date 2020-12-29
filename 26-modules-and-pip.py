9efafb27e7604c6f258325a79718032751bf96d1
Jay T <jaytula@gmail.com>
2020-12-29 01:06:42 -0800

    Add 26-modules-and-pip.py.  In 3:43:56

---
+++ 26-modules-and-pip.py
@@ -0,0 +1,22 @@
+# Modules
+
+# modules are python files that we can import into our python file
+# - re-use code
+
+import useful_tools
+import docx
+
+print(useful_tools.roll_dice(10))
+
+# List of python modules: https://docs.python.org/3/py-modindex.html
+
+# Two types of modules
+# - built-in
+# - external modules - External Libraries -> Python 3.8 -> Lib
+
+
+# And Third-Party modules
+# - Search on google
+# - `pip install python-module`
+# - `pip` comes with Python 3
+# - Installed at `venv/Lib/site-packages`
