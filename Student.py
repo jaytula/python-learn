6455114d4fbcd829db1994f1ab3e9659e9e4e9a3
Jay T <jaytula@gmail.com>
2021-01-01 17:23:45 -0800

    Add 29-object-functions.py. In 4:12:37

--- Student.py
+++ Student.py
@@ -1,6 +1,9 @@
 class Student:
-    def __init__(self, name, major, gpa, is_on_probation):
+    def __init__(self, name, major, gpa):
         self.name = name
         self.major = major
         self.gpa = gpa
-        self.is_on_probation = is_on_probation
+        # self.is_on_probation = is_on_probation
+
+    def on_honor_roll(self):
+        return self.gpa >= 3.5
